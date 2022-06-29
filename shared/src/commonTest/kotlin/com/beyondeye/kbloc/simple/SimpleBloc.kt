55e4d259920e7adb75e58bca54fc417d809f2467
Dario Elyasy <dario-e@beyond-eye.com>
2022-06-29 18:15:46 +0300

    SimpleBloc implemented

--- shared/src/commonTest/kotlin/com/beyondeye/kbloc/simple/SimpleBloc.kt
+++ shared/src/commonTest/kotlin/com/beyondeye/kbloc/simple/SimpleBloc.kt
@@ -3,10 +3,9 @@ package com.beyondeye.kbloc.simple
 import com.beyondeye.kbloc.core.Bloc
 import kotlinx.coroutines.CoroutineScope
 
-class SimpleBloc :Bloc<Any,String> {
-    constructor(cscope_stateUpdate: CoroutineScope,
-                useReferenceEqualityForStateChanges: Boolean
-    ) : super("", cscope_stateUpdate, useReferenceEqualityForStateChanges) {
+class SimpleBloc (cscope_stateUpdate: CoroutineScope, useReferenceEqualityForStateChanges: Boolean) :
+    Bloc<Any, String>("", cscope_stateUpdate, useReferenceEqualityForStateChanges) {
+    init {
         on<String>{ _, emitter ->
             //TODO: the original DART code is on<String>((_, emit) => emit('data'));
             //   that apparently don't use the emit parameter, but instead use the bloc.emit() method: this is potentially an error

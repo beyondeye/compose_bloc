3976b93f12209345dd3e608880476517c02d3e6d
Dario Elyasy <dario-e@beyond-eye.com>
2022-06-30 14:05:20 +0300

    counter_error_bloc.kt implemented

--- shared/src/commonTest/kotlin/com/beyondeye/kbloc/counter/counter_error_bloc.kt
+++ shared/src/commonTest/kotlin/com/beyondeye/kbloc/counter/counter_error_bloc.kt
@@ -1,23 +1,19 @@
 package com.beyondeye.kbloc.counter
 
-/*
-import 'package:bloc/bloc.dart';
+import com.beyondeye.kbloc.core.Bloc
+import com.beyondeye.kbloc.core.Emitter
+import kotlinx.coroutines.CoroutineScope
 
-import '../counter/counter_bloc.dart';
-
-class CounterErrorBloc extends Bloc<CounterEvent, int> {
-  CounterErrorBloc() : super(0) {
-    on<CounterEvent>(_onCounterEvent);
-  }
-
-  void _onCounterEvent(CounterEvent event, Emitter<int> emit) {
-    switch (event) {
-      case CounterEvent.decrement:
-        return emit(state - 1);
-      case CounterEvent.increment:
-        throw Error();
+class CountErrorBloc(cscope:CoroutineScope) : Bloc<CounterEvent,Int>(cscope,0) {
+    init {
+        on<CounterEvent> { event, emit ->
+            _onCounterEvent(event,emit)
+        }
     }
-  }
+    fun _onCounterEvent(event:CounterEvent,emit:Emitter<Int>) {
+        when(event) {
+            CounterEvent.decrement -> emit(state - 1)
+            CounterEvent.increment -> throw Exception()
+        }
+    }
 }
-
- */

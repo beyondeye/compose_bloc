3ee944691363c7498a2d4b1850ade37189ab3aba
Dario Elyasy <dario-e@beyond-eye.com>
2022-06-30 12:52:19 +0300

    counter_bloc.kt implemented

--- shared/src/commonTest/kotlin/com/beyondeye/kbloc/counter/counter_bloc.kt
+++ shared/src/commonTest/kotlin/com/beyondeye/kbloc/counter/counter_bloc.kt
@@ -1,53 +1,50 @@
 package com.beyondeye.kbloc.counter
 
-/*
-import 'package:bloc/bloc.dart';
+import com.beyondeye.kbloc.core.Bloc
+import com.beyondeye.kbloc.core.Emitter
+import com.beyondeye.kbloc.core.Transition
+import kotlinx.coroutines.CoroutineScope
 
-typedef OnEventCallback = Function(CounterEvent);
-typedef OnTransitionCallback = Function(Transition<CounterEvent, int>);
-typedef OnErrorCallback = Function(Object error, StackTrace? stackTrace);
 
-enum CounterEvent { increment, decrement }
-
-class CounterBloc extends Bloc<CounterEvent, int> {
-  CounterBloc({
-    this.onEventCallback,
-    this.onTransitionCallback,
-    this.onErrorCallback,
-  }) : super(0) {
-    on<CounterEvent>(_onCounterEvent);
-  }
-
-  final OnEventCallback? onEventCallback;
-  final OnTransitionCallback? onTransitionCallback;
-  final OnErrorCallback? onErrorCallback;
-
-  @override
-  void onEvent(CounterEvent event) {
-    super.onEvent(event);
-    onEventCallback?.call(event);
-  }
-
-  @override
-  void onTransition(Transition<CounterEvent, int> transition) {
-    super.onTransition(transition);
-    onTransitionCallback?.call(transition);
-  }
-
-  @override
-  void onError(Object error, StackTrace stackTrace) {
-    onErrorCallback?.call(error, stackTrace);
-    super.onError(error, stackTrace);
-  }
-
-  void _onCounterEvent(CounterEvent event, Emitter<int> emit) {
-    switch (event) {
-      case CounterEvent.increment:
-        return emit(state + 1);
-      case CounterEvent.decrement:
-        return emit(state - 1);
-    }
-  }
+enum class CounterEvent {
+    increment,
+    decrement
 }
+typealias OnEventCallback = (CounterEvent)->Unit
+typealias onTransitionCallback = (Transition<CounterEvent,Int>)-> Unit
+typealias onErrorCallback = (Throwable)-> Unit
 
- */
+class CounterBloc(cscope:CoroutineScope,
+                  val onEventCallback: OnEventCallback?=null,
+                  val onTransitionCallback: onTransitionCallback?=null,
+                  val onErrorCallback: onErrorCallback?=null
+)            :Bloc<CounterEvent,Int>(cscope,0)
+{
+    init {
+        on<CounterEvent> { event, emit ->
+            _onCounterEvent(event,emit)
+        }
+    }
+    override fun onEvent(event: CounterEvent) {
+        super.onEvent(event)
+        onEventCallback?.invoke(event)
+    }
+
+    override fun onTransition(transition: Transition<CounterEvent, Int>) {
+        super.onTransition(transition)
+        onTransitionCallback?.invoke(transition)
+    }
+
+    override fun onError(error: Throwable) {
+        super.onError(error)
+        onErrorCallback?.invoke(error)
+    }
+    private fun _onCounterEvent(event: CounterEvent, emit: Emitter<Int>) {
+        when(event) {
+            CounterEvent.increment -> emit(state + 1)
+            CounterEvent.decrement -> emit(state -1)
+        }
+        TODO("Not yet implemented")
+    }
+
+}

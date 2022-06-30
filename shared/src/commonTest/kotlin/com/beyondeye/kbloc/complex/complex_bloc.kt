d771a30f8f790c23acddafcf7ae2954090aaf6c0
Dario Elyasy <dario-e@beyond-eye.com>
2022-06-30 10:15:28 +0300

    added skeleton for complex_bloc.kt

---
+++ shared/src/commonTest/kotlin/com/beyondeye/kbloc/complex/complex_bloc.kt
@@ -0,0 +1,33 @@
+package com.beyondeye.kbloc.complex
+
+/*
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:stream_transform/stream_transform.dart';
+
+part 'complex_event.dart';
+part 'complex_state.dart';
+
+const _delay = Duration(milliseconds: 100);
+
+class ComplexBloc extends Bloc<ComplexEvent, ComplexState> {
+  ComplexBloc() : super(ComplexStateA()) {
+    on<ComplexEventA>((_, emit) => emit(ComplexStateA()));
+    on<ComplexEventB>((_, emit) => emit(ComplexStateB()));
+    on<ComplexEventC>(
+      (_, emit) => Future<void>.delayed(_delay, () => emit(ComplexStateC())),
+    );
+    on<ComplexEventD>(
+      (_, emit) => Future<void>.delayed(_delay, () => emit(ComplexStateD())),
+    );
+  }
+
+  @override
+  Stream<ComplexState> get stream {
+    return super.stream.debounce(const Duration(milliseconds: 50));
+  }
+}
+
+ */

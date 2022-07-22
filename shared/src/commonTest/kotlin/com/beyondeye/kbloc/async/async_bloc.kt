867a654302b69e1fa029ee721c2a2c015d6e7b7c
Dario Elyasy <dario-e@beyond-eye.com>
2022-07-22 15:26:38 +0300

    added implementation of asyncExpand() and concurrentAsyncExpand()

--- shared/src/commonTest/kotlin/com/beyondeye/kbloc/async/async_bloc.kt
+++ shared/src/commonTest/kotlin/com/beyondeye/kbloc/async/async_bloc.kt
@@ -4,6 +4,8 @@ import com.beyondeye.kbloc.core.Bloc
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flatMapMerge
 
 class AsyncBloc(cscope: CoroutineScope) : Bloc<AsyncEvent,AsyncState>(cscope,AsyncState.initial())
 {
@@ -35,7 +37,40 @@ class AsyncBloc(cscope: CoroutineScope) : Bloc<AsyncEvent,AsyncState>(cscope,Asy
 * The returned stream is a broadcast stream if this stream is.
  */
 public fun <E,T> Flow<T>.asyncExpand(convert: (T) -> Flow<E>): Flow<E> {
-    TODO("Not yet implemented")
+    //see also https://kotlinlang.org/api/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines.flow/flat-map-concat.html
+    return flatMapConcat(convert)
+}
+
+
+/// Like [asyncExpand] but the [convert] callback may be called for an element
+/// before the [Stream] emitted by the previous element has closed.
+///
+/// Events on the result stream will be emitted in the order they are emitted
+/// by the sub streams, which may not match the order of the original stream.
+///
+/// Errors from [convert], the source stream, or any of the sub streams are
+/// forwarded to the result stream.
+///
+/// The result stream will not close until the source stream closes and all
+/// sub streams have closed.
+///
+/// If the source stream is a broadcast stream, the result will be as well,
+/// regardless of the types of streams created by [convert]. In this case,
+/// some care should be taken:
+/// -  If [convert] returns a single subscription stream it may be listened to
+/// and never canceled.
+/// -  For any period of time where there are no listeners on the result
+/// stream, any sub streams from previously emitted events will be ignored,
+/// regardless of whether they emit further events after a listener is added
+/// back.
+///
+/// See also:
+///
+///  * [switchMap], which cancels subscriptions to the previous sub
+///    stream instead of concurrently emitting events from all sub streams.
+public fun <T> Flow<T>.concurrentAsyncExpand(mapper: (T) -> Flow<T>): Flow<T> {
+    //see also https://kotlinlang.org/api/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines.flow/flat-map-merge.html
+    return flatMapMerge(transform = mapper)
 }
 
 
--- shared/src/commonTest/kotlin/com/beyondeye/kbloc/counter/merge_bloc.kt
+++ shared/src/commonTest/kotlin/com/beyondeye/kbloc/counter/merge_bloc.kt
@@ -1,9 +1,11 @@
 package com.beyondeye.kbloc.counter
 
+import com.beyondeye.kbloc.async.concurrentAsyncExpand
 import com.beyondeye.kbloc.core.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flatMapMerge
 import kotlinx.coroutines.flow.merge
 
 val customTransformer:EventTransformer<CounterEvent> = { events, mapper ->
@@ -15,36 +17,7 @@ val customTransformer:EventTransformer<CounterEvent> = { events, mapper ->
     merge(nonDebounceStream,debounceStream).concurrentAsyncExpand(mapper)
 }
 
-/// Like [asyncExpand] but the [convert] callback may be called for an element
-/// before the [Stream] emitted by the previous element has closed.
-///
-/// Events on the result stream will be emitted in the order they are emitted
-/// by the sub streams, which may not match the order of the original stream.
-///
-/// Errors from [convert], the source stream, or any of the sub streams are
-/// forwarded to the result stream.
-///
-/// The result stream will not close until the source stream closes and all
-/// sub streams have closed.
-///
-/// If the source stream is a broadcast stream, the result will be as well,
-/// regardless of the types of streams created by [convert]. In this case,
-/// some care should be taken:
-/// -  If [convert] returns a single subscription stream it may be listened to
-/// and never canceled.
-/// -  For any period of time where there are no listeners on the result
-/// stream, any sub streams from previously emitted events will be ignored,
-/// regardless of whether they emit further events after a listener is added
-/// back.
-///
-/// See also:
-///
-///  * [switchMap], which cancels subscriptions to the previous sub
-///    stream instead of concurrently emitting events from all sub streams.
-private fun <T> Flow<T>.concurrentAsyncExpand(mapper: (T) -> Flow<T>): Flow<T> {
 
-    TODO()
-}
 
 class MergeBloc(cscope:CoroutineScope,val  onTransitionCallback: onTransitionCallback?=null) : Bloc<CounterEvent, Int>(cscope,0) {
     init {

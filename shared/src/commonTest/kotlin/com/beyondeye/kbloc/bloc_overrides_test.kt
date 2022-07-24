7ce0145ed840022e5b430cefdfd0a9d01e39be9e
Dario Elyasy <dario-e@beyond-eye.com>
2022-07-24 11:35:05 +0300

    bloc_overrides_test.kt implementation completed

--- shared/src/commonTest/kotlin/com/beyondeye/kbloc/bloc_overrides_test.kt
+++ shared/src/commonTest/kotlin/com/beyondeye/kbloc/bloc_overrides_test.kt
@@ -1,118 +1,114 @@
-package com.beyondeye.kbloc/*
-import 'package:bloc/bloc.dart';
-import 'package:mocktail/mocktail.dart';
-import 'package:test/test.dart';
+package com.beyondeye.kbloc
 
-class FakeBlocObserver extends Fake implements BlocObserver {}
+import com.beyondeye.kbloc.async.asyncExpand
+import com.beyondeye.kbloc.core.BlocObserver
+import com.beyondeye.kbloc.core.BlocOverrides
+import com.beyondeye.kbloc.core.EventTransformer
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+class FakeBlocObserver<T:Any> :BlocObserver<T> { }
 
-void main() {
-  group('BlocOverrides', () {
-    group('runZoned', () {
-      test('uses default BlocObserver when not specified', () {
-        BlocOverrides.runZoned(() {
-          final overrides = BlocOverrides.current;
-          expect(overrides!.blocObserver, isA<BlocObserver>());
-        });
-      });
+class BlocOverridesTest {
+    @Test
+    fun BlocOverrides_uses_default_BlocObserver_when_not_specified() {
+        BlocOverrides.runZoned {
+            val overrides=BlocOverrides.current
+            assertTrue { overrides?.blocObserver is BlocObserver }
+        }
+    }
+    @Test
+    fun BlocOverrides_uses_default_EventTransformer_when_not_specified() {
+        BlocOverrides.runZoned {
+            val overrides=BlocOverrides.current
+            assertTrue { overrides?.eventTransformer is EventTransformer<Any> }
+        }
+    }
+    @Test
+    fun BlocOverrides_uses_custom_BlocObserver_when_specified() {
+        val blocObserver=FakeBlocObserver<Any>()
+        BlocOverrides.runZoned(blocObserver=blocObserver) {
+            val overrides=BlocOverrides.current
+            assertTrue { overrides?.blocObserver === blocObserver }
+        }
+    }
 
-      test('uses default EventTransformer when not specified', () {
-        BlocOverrides.runZoned(() {
-          final overrides = BlocOverrides.current;
-          expect(overrides!.eventTransformer, isA<EventTransformer>());
-        });
-      });
+    @Test
+    fun BlocOverrides_uses_custom_EventTransformer_when_specified() {
+        val eventTransformer:EventTransformer<Any> = {events, mapper ->
+            events.asyncExpand(mapper)
+        }
+        BlocOverrides.runZoned(eventTransformer = eventTransformer) {
+            val overrides=BlocOverrides.current
+            assertTrue { overrides?.eventTransformer === eventTransformer }
+        }
+    }
 
-      test('uses custom BlocObserver when specified', () {
-        final blocObserver = FakeBlocObserver();
-        BlocOverrides.runZoned(() {
-          final overrides = BlocOverrides.current;
-          expect(overrides!.blocObserver, equals(blocObserver));
-        }, blocObserver: blocObserver);
-      });
+    @Test
+    fun BlocOverrides_uses_current_BlocObserver_when_not_specified() {
+        val blocObserver=FakeBlocObserver<Any>()
+        BlocOverrides.runZoned(blocObserver=blocObserver) {
+            BlocOverrides.runZoned {
+                val overrides=BlocOverrides.current
+                assertTrue { overrides?.blocObserver === blocObserver }
+            }
+        }
+    }
+    @Test
+    fun BlocOverrides_uses_current_EventTransformer_when_not_specified() {
+        val eventTransformer:EventTransformer<Any> = {events, mapper ->
+            events.asyncExpand(mapper)
+        }
+        BlocOverrides.runZoned(eventTransformer = eventTransformer) {
+            BlocOverrides.runZoned {
+                val overrides=BlocOverrides.current
+                assertTrue { overrides?.eventTransformer === eventTransformer }
+            }
+        }
+    }
+    @Test
+    fun BlocOverrides_uses_nested_BlocObserver_when_specified() {
+        val rootBlocObserver=FakeBlocObserver<Any>()
+        BlocOverrides.runZoned(blocObserver=rootBlocObserver) {
+            val overrides=BlocOverrides.current
+            assertTrue { overrides?.blocObserver === rootBlocObserver }
+            val nestedBlocObserver=FakeBlocObserver<Any>()
+            BlocOverrides.runZoned(blocObserver = nestedBlocObserver) {
+                val overrides=BlocOverrides.current
+                assertTrue { overrides?.blocObserver === nestedBlocObserver }
+            }
+        }
+    }
 
-      test('uses custom EventTransformer when specified', () {
-        final eventTransformer = (Stream events, EventMapper mapper) {
-          return events.asyncExpand<dynamic>(mapper);
-        };
-        BlocOverrides.runZoned(() {
-          final overrides = BlocOverrides.current;
-          expect(overrides!.eventTransformer, equals(eventTransformer));
-        }, eventTransformer: eventTransformer);
-      });
-
-      test(
-          'uses current BlocObserver when not specified '
-          'and zone already contains a BlocObserver', () {
-        final blocObserver = FakeBlocObserver();
-        BlocOverrides.runZoned(() {
-          BlocOverrides.runZoned(() {
-            final overrides = BlocOverrides.current;
-            expect(overrides!.blocObserver, equals(blocObserver));
-          });
-        }, blocObserver: blocObserver);
-      });
-
-      test(
-          'uses current EventTransformer when not specified '
-          'and zone already contains an EventTransformer', () {
-        final eventTransformer = (Stream events, EventMapper mapper) {
-          return events.asyncExpand<dynamic>(mapper);
-        };
-        BlocOverrides.runZoned(() {
-          BlocOverrides.runZoned(() {
-            final overrides = BlocOverrides.current;
-            expect(overrides!.eventTransformer, equals(eventTransformer));
-          });
-        }, eventTransformer: eventTransformer);
-      });
-
-      test(
-          'uses nested BlocObserver when specified '
-          'and zone already contains a BlocObserver', () {
-        final rootBlocObserver = FakeBlocObserver();
-        BlocOverrides.runZoned(() {
-          final nestedBlocObserver = FakeBlocObserver();
-          final overrides = BlocOverrides.current;
-          expect(overrides!.blocObserver, equals(rootBlocObserver));
-          BlocOverrides.runZoned(() {
-            final overrides = BlocOverrides.current;
-            expect(overrides!.blocObserver, equals(nestedBlocObserver));
-          }, blocObserver: nestedBlocObserver);
-        }, blocObserver: rootBlocObserver);
-      });
-
-      test(
-          'uses nested EventTransformer when specified '
-          'and zone already contains an EventTransformer', () {
-        final rootEventTransformer = (Stream events, EventMapper mapper) {
-          return events.asyncExpand<dynamic>(mapper);
-        };
-        BlocOverrides.runZoned(() {
-          final nestedEventTransformer = (Stream events, EventMapper mapper) {
-            return events.asyncExpand<dynamic>(mapper);
-          };
-          final overrides = BlocOverrides.current;
-          expect(overrides!.eventTransformer, equals(rootEventTransformer));
-          BlocOverrides.runZoned(() {
-            final overrides = BlocOverrides.current;
-            expect(overrides!.eventTransformer, equals(nestedEventTransformer));
-          }, eventTransformer: nestedEventTransformer);
-        }, eventTransformer: rootEventTransformer);
-      });
-
-      test('overrides cannot be mutated after zone is created', () {
-        final originalBlocObserver = FakeBlocObserver();
-        final otherBlocObserver = FakeBlocObserver();
-        var blocObserver = originalBlocObserver;
-        BlocOverrides.runZoned(() {
-          blocObserver = otherBlocObserver;
-          final overrides = BlocOverrides.current!;
-          expect(overrides.blocObserver, equals(originalBlocObserver));
-          expect(overrides.blocObserver, isNot(equals(otherBlocObserver)));
-        }, blocObserver: blocObserver);
-      });
-    });
-  });
+    @Test
+    fun BlocOverrides_uses_nested_EventTransformer_when_specified() {
+        val rootEventTransformer:EventTransformer<Any> = {events, mapper ->
+            events.asyncExpand(mapper)
+        }
+        BlocOverrides.runZoned(eventTransformer = rootEventTransformer) {
+            val overrides = BlocOverrides.current
+            assertTrue { overrides?.eventTransformer === rootEventTransformer }
+            val nestedEventTransformer: EventTransformer<Any> = { events, mapper ->
+                events.asyncExpand(mapper)
+            }
+            BlocOverrides.runZoned(eventTransformer = nestedEventTransformer) {
+                val overrides = BlocOverrides.current
+                assertTrue { overrides?.eventTransformer === nestedEventTransformer }
+            }
+        }
+    }
+    //TODO: I cannot implement this test
+    @Test
+    fun BlocOverrides_overrides_cannot_be_mutated_after_zone_created() {
+        val originalBlocObserver=FakeBlocObserver<Any>()
+        val otherBlocObserver=FakeBlocObserver<Any>()
+        var blocObserver=originalBlocObserver
+        BlocOverrides.runZoned(blocObserver=blocObserver)
+        {
+            blocObserver=otherBlocObserver
+            val overrides=BlocOverrides.current
+            assertTrue(originalBlocObserver===overrides?.blocObserver)
+            assertTrue { overrides?.blocObserver!==otherBlocObserver }
+        }
+    }
 }
-
- */

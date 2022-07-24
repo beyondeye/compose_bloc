82dbf801a1af95ace3c53764661b5aa2a9f0bbc2
Dario Elyasy <dario-e@beyond-eye.com>
2022-07-24 17:57:31 +0300

    transition_test.kt implementation completed

--- shared/src/commonTest/kotlin/com/beyondeye/kbloc/transition_test.kt
+++ shared/src/commonTest/kotlin/com/beyondeye/kbloc/transition_test.kt
@@ -1,80 +1,91 @@
 package com.beyondeye.kbloc
 
+import com.beyondeye.kbloc.core.Change
+import com.beyondeye.kbloc.core.Transition
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+
+interface TransitionEvent
+interface TransitionState
+class SimpleTransitionEvent:TransitionEvent
+class SimpleTransitionState:TransitionState
+data class CounterEvent_(val eventData:String):TransitionEvent
+data class CounterState_(val count:Int):TransitionState
+
+class ChangeAndTransitionTests {
+    @Test
+    fun change_constructor_should_return_normally_when_initialized_with_all_required_parameters() {
+        val c= Change<Int>(0,1)
+    }
+    @Test
+    fun change_equal_operator_should_return_true_if_2_changes_are_equal() {
+        val changeA=Change<Int>(0,1)
+        val changeB=Change<Int>(0,1)
+        assertEquals(changeA,changeB)
+    }
+    @Test
+    fun change_equal_operator_should_return_false_if_2_changes_are_not_equal() {
+        val changeA=Change<Int>(0,1)
+        val changeB=Change<Int>(0,-1)
+        assertNotEquals(changeA,changeB)
+    }
+
+    @Test
+    fun transition_constructor_should_not_throw_assertion_when_initialied_with_null_state() {
+        val t= Transition<TransitionEvent,TransitionState?>(null,SimpleTransitionEvent(),SimpleTransitionState())
+    }
+
+    @Test
+    fun transition_constructor_should_not_throw_assertion_when_initialied_with_null_event() {
+        val t= Transition<TransitionEvent?,TransitionState>(SimpleTransitionState(),null,SimpleTransitionState())
+    }
+
+    @Test
+    fun transition_constructor_should_not_throw_assertion_when_initialied_with_null_nextstate() {
+        val t= Transition<TransitionEvent,TransitionState?>(SimpleTransitionState(),SimpleTransitionEvent(),null)
+    }
+    @Test
+    fun transition_constructor_should_not_throw_assertion_when_initialied_with_all_required_params() {
+        val t= Transition<TransitionEvent?,TransitionState>(SimpleTransitionState(),SimpleTransitionEvent(),SimpleTransitionState())
+    }
+
+    @Test
+    fun transition_equals_should_return_true_if_2_transitions_are_equal() {
+        val transitionA = Transition<CounterEvent_, CounterState_>(
+            CounterState_(0),
+            CounterEvent_("increment"),
+            CounterState_(1)
+        )
+        val transitionB = Transition<CounterEvent_, CounterState_>(
+            CounterState_(0),
+            CounterEvent_("increment"),
+            CounterState_(1)
+        )
+        assertEquals(transitionA,transitionB)
+    }
+
+    @Test
+    fun transition_equals_should_return_false_if_2_transitions_are_not_equal() {
+        val transitionA = Transition<CounterEvent_, CounterState_>(
+            CounterState_(0),
+            CounterEvent_("increment"),
+            CounterState_(1)
+        )
+        val transitionB = Transition<CounterEvent_, CounterState_>(
+            CounterState_(1),
+            CounterEvent_("decrement"),
+            CounterState_(0)
+        )
+        assertNotEquals(transitionA,transitionB)
+    }
+}
+
 /*
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-import 'package:test/test.dart';
-
-@immutable
-abstract class TransitionEvent {}
-
-@immutable
-abstract class TransitionState {}
-
-class SimpleTransitionEvent extends TransitionEvent {}
-
-class SimpleTransitionState extends TransitionState {}
-
-class CounterEvent extends TransitionEvent {
-  CounterEvent(this.eventData);
-
-  final String eventData;
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is CounterEvent &&
-          runtimeType == other.runtimeType &&
-          eventData == other.eventData;
-
-  @override
-  int get hashCode => eventData.hashCode;
-}
-
-class CounterState extends TransitionState {
-  CounterState(this.count);
-
-  final int count;
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is CounterState &&
-          runtimeType == other.runtimeType &&
-          count == other.count;
-
-  @override
-  int get hashCode => count.hashCode;
-}
 
 void main() {
   group('Change Tests', () {
-    group('constructor', () {
-      test(
-          'should return normally when initialized with '
-          'all required parameters', () {
-        expect(
-          () => const Change<int>(currentState: 0, nextState: 1),
-          returnsNormally,
-        );
-      });
-    });
 
-    group('== operator', () {
-      test('should return true if 2 Changes are equal', () {
-        final changeA = const Change<int>(currentState: 0, nextState: 1);
-        final changeB = const Change<int>(currentState: 0, nextState: 1);
-
-        expect(changeA == changeB, isTrue);
-      });
-
-      test('should return false if 2 Changes are not equal', () {
-        final changeA = const Change<int>(currentState: 0, nextState: 1);
-        final changeB = const Change<int>(currentState: 0, nextState: -1);
-
-        expect(changeA == changeB, isFalse);
-      });
-    });
 
     group('hashCode', () {
       test('should return correct hashCode', () {
@@ -101,94 +112,7 @@ void main() {
 
   group('Transition Tests', () {
     group('constructor', () {
-      test(
-          'should not throw assertion error when initialized '
-          'with a null currentState', () {
-        expect(
-          () => Transition<TransitionEvent, TransitionState?>(
-            currentState: null,
-            event: SimpleTransitionEvent(),
-            nextState: SimpleTransitionState(),
-          ),
-          isNot(throwsA(isA<AssertionError>())),
-        );
-      });
 
-      test(
-          'should not throw assertion error when initialized with a null event',
-          () {
-        expect(
-          () => Transition<TransitionEvent?, TransitionState>(
-            currentState: SimpleTransitionState(),
-            event: null,
-            nextState: SimpleTransitionState(),
-          ),
-          isNot(throwsA(isA<AssertionError>())),
-        );
-      });
-
-      test(
-          'should not throw assertion error '
-          'when initialized with a null nextState', () {
-        expect(
-          () => Transition<TransitionEvent, TransitionState?>(
-            currentState: SimpleTransitionState(),
-            event: SimpleTransitionEvent(),
-            nextState: null,
-          ),
-          isNot(throwsA(isA<AssertionError>())),
-        );
-      });
-
-      test(
-          'should not throw assertion error when initialized with '
-          'all required parameters', () {
-        try {
-          Transition<TransitionEvent, TransitionState>(
-            currentState: SimpleTransitionState(),
-            event: SimpleTransitionEvent(),
-            nextState: SimpleTransitionState(),
-          );
-        } catch (_) {
-          fail(
-            'should not throw error when initialized '
-            'with all required parameters',
-          );
-        }
-      });
-    });
-
-    group('== operator', () {
-      test('should return true if 2 Transitions are equal', () {
-        final transitionA = Transition<CounterEvent, CounterState>(
-          currentState: CounterState(0),
-          event: CounterEvent('increment'),
-          nextState: CounterState(1),
-        );
-        final transitionB = Transition<CounterEvent, CounterState>(
-          currentState: CounterState(0),
-          event: CounterEvent('increment'),
-          nextState: CounterState(1),
-        );
-
-        expect(transitionA == transitionB, true);
-      });
-
-      test('should return false if 2 Transitions are not equal', () {
-        final transitionA = Transition<CounterEvent, CounterState>(
-          currentState: CounterState(0),
-          event: CounterEvent('increment'),
-          nextState: CounterState(1),
-        );
-        final transitionB = Transition<CounterEvent, CounterState>(
-          currentState: CounterState(1),
-          event: CounterEvent('decrement'),
-          nextState: CounterState(0),
-        );
-
-        expect(transitionA == transitionB, false);
-      });
-    });
 
     group('hashCode', () {
       test('should return correct hashCode', () {

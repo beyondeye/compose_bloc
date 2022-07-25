6ffffd9ab71d2834059641d17d2c71ba9c8e62f8
Dario Elyasy <dario-e@beyond-eye.com>
2022-07-25 14:57:49 +0300

    added dep for module compose_bloc from module "shared"

--- compose_bloc/build.gradle.kts
+++ compose_bloc/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
 //    implementation("androidx.core:core-ktx:1.7.0")
 //    implementation("androidx.appcompat:appcompat:1.4.2")
 //    implementation("com.google.android.material:material:1.6.1")
-
+    api(project(":shared"))
     compileOnly("androidx.compose.runtime:runtime:${rootProject.extra["compose_version"]}")
 //    implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
 //    implementation("androidx.compose.ui:ui-tooling-preview:${rootProject.extra["compose_version"]}")

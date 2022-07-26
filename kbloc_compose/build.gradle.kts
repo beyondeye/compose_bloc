44c76e8402b98bd626ba0d216ba7c5a53f192115
Dario Elyasy <dario-e@beyond-eye.com>
2022-07-26 15:52:32 +0300

    added missing dep for kbloc_compose from kbloc_core

--- kbloc_compose/build.gradle.kts
+++ kbloc_compose/build.gradle.kts
@@ -40,14 +40,15 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.7.0")
+    api(project(":kbloc_core"))
+    implementation("androidx.core:core-ktx:1.8.0")
     compileOnly("androidx.compose.runtime:runtime:${rootProject.extra["compose_version"]}")
     compileOnly("androidx.compose.runtime:runtime-saveable:${rootProject.extra["compose_version"]}")
 
     //implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
     //implementation("androidx.compose.ui:ui-tooling-preview:${rootProject.extra["compose_version"]}")
     //    implementation("androidx.activity:activity-compose:1.3.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")

d2e89a27a3dff3ece08f743164b602bbd7f4fd4e
Dario Elyasy <dario-e@beyond-eye.com>
2022-08-02 13:13:13 +0300

    add option to define BlocProvider in MultiBlocProvider using externallyProvidedBlock, and other small fixes

--- kbloc_compose/src/main/kotlin/com/beyondeye/kbloc/compose/bloc/MultiBlocProvider.kt
+++ kbloc_compose/src/main/kotlin/com/beyondeye/kbloc/compose/bloc/MultiBlocProvider.kt
@@ -2,7 +2,9 @@ package com.beyondeye.kbloc.compose.bloc
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisallowComposableCalls
+import androidx.compose.runtime.remember
 import com.beyondeye.kbloc.compose.bloc.internals.BindBlocs
+import com.beyondeye.kbloc.compose.bloc.internals.BlocStore
 import com.beyondeye.kbloc.compose.bloc.internals.rememberBloc
 import com.beyondeye.kbloc.compose.screen.Screen
 import com.beyondeye.kbloc.core.BlocBase
@@ -26,80 +28,29 @@ public fun Screen.MultiBlocProvider():_BlocProviderList {
  */
 class _BlocProviderList(val screen: Screen, val blist:MutableList<Triple<BlocBase<*>,String,String>> = mutableListOf()) {
     @Composable
-    public inline fun <reified BlocA: BlocBase<BlocAState>,BlocAState:Any> BlocProvider(
-        tag: String? = null,
+    public inline fun <reified BlocA: BlocBase<*>> BlocProvider(
+        blocTag: String? = null,
         crossinline create: @DisallowComposableCalls (cscope: CoroutineScope) -> BlocA
     )    : _BlocProviderList
     {
-        val (b,bkey)=screen.rememberBloc(tag,create)
-        blist.add(Triple(b,tag?:"",bkey))
+        val (b,bkey)=screen.rememberBloc(blocTag,create)
+        blist.add(Triple(b,blocTag?:"",bkey))
         return this
     }
     @Composable
-    public fun forContent(content:@Composable ()->Unit) {
+    public inline fun <reified BlocA: BlocBase<*>> BlocProvider(
+        blocTag: String? = null,
+        externallyProvidedBlock:BlocA,
+    ): _BlocProviderList {
+        val b= remember { externallyProvidedBlock }
+        val bkey = BlocStore.getBlocKeyForUnboundBloc<BlocA>(blocTag)
+        blist.add(Triple(b,blocTag?:"",bkey))
+        return this
+    }
+
+    @Composable
+    public fun ForContent(content:@Composable ()->Unit) {
         BindBlocs(blist,content)
         blist.clear()
     }
 }
-
-//
-/*
-import 'package:flutter/widgets.dart';
-import 'package:flutter_bloc/src/bloc_provider.dart';
-import 'package:provider/provider.dart';
-
-/// {@template multi_bloc_provider}
-/// Merges multiple [BlocProvider] widgets into one widget tree.
-///
-/// [MultiBlocProvider] improves the readability and eliminates the need
-/// to nest multiple [BlocProvider]s.
-///
-/// By using [MultiBlocProvider] we can go from:
-///
-/// ```dart
-/// BlocProvider<BlocA>(
-///   create: (BuildContext context) => BlocA(),
-///   child: BlocProvider<BlocB>(
-///     create: (BuildContext context) => BlocB(),
-///     child: BlocProvider<BlocC>(
-///       create: (BuildContext context) => BlocC(),
-///       child: ChildA(),
-///     )
-///   )
-/// )
-/// ```
-///
-/// to:
-///
-/// ```dart
-/// MultiBlocProvider(
-///   providers: [
-///     BlocProvider<BlocA>(
-///       create: (BuildContext context) => BlocA(),
-///     ),
-///     BlocProvider<BlocB>(
-///       create: (BuildContext context) => BlocB(),
-///     ),
-///     BlocProvider<BlocC>(
-///       create: (BuildContext context) => BlocC(),
-///     ),
-///   ],
-///   child: ChildA(),
-/// )
-/// ```
-///
-/// [MultiBlocProvider] converts the [BlocProvider] list into a tree of nested
-/// [BlocProvider] widgets.
-/// As a result, the only advantage of using [MultiBlocProvider] is improved
-/// readability due to the reduction in nesting and boilerplate.
-/// {@endtemplate}
-class MultiBlocProvider extends MultiProvider {
-  /// {@macro multi_bloc_provider}
-  MultiBlocProvider({
-    Key? key,
-    required List<BlocProviderSingleChildWidget> providers,
-    required Widget child,
-  }) : super(key: key, providers: providers, child: child);
-}
-
- */

08f2f25f7ac083425b911bdd022e347c7e3e0223
Dario Elyasy <dario-e@beyond-eye.com>
2022-08-02 14:48:28 +0300

    finalized javadocs for BlocSelector

--- kbloc_compose/src/main/kotlin/com/beyondeye/kbloc/compose/bloc/BlocSelector.kt
+++ kbloc_compose/src/main/kotlin/com/beyondeye/kbloc/compose/bloc/BlocSelector.kt
@@ -5,36 +5,35 @@ import com.beyondeye.kbloc.compose.screen.Screen
 import com.beyondeye.kbloc.core.BlocBase
 import kotlinx.coroutines.flow.map
 
-/// Signature for the `selector` function which
-/// is responsible for returning a selected value, [T], based on [state].
-typealias BlocWidgetSelector<S, T> = (S)->T
+/**
+ * Signature for the `selector` function which
+ * is responsible for returning a selected value  T, based on S.
+ * NOTE: this was named BlocWidgetSelector in flutter_bloc
+ * TODO: integrate BlocSelectorFn with reduks selectors that automatically check for state changes before emitting selected state
+ */
+typealias BlocSelectorFn<S, T> = (S)->T
 
 /**
  *
- * {@template bloc_selector}
  * [BlocSelector] is analogous to [BlocBuilder] but allows developers to
  * filter updates by selecting a new value based on the bloc state.
- * Unnecessary builds are prevented if the selected value does not change.
+ * Unnecessary recompositions of [content] are prevented if the selected value does not change.
+ *
+ * An optional [blocTag] parameter can be specified in order to identify a specific
+ * bloc instance in case there is more than one instance of a bloc of same type
+ * registered for the current composable subtree (see [BlocProvider])
+ * [blocTag] parameter is not present in the original flutter_bloc implementation
  *
  * **Note**: the selected value must be immutable in order for [BlocSelector]
- * to accurately determine whether [builder] should be called again.
+ * to accurately determine whether [content] should be called again.
+ * TODO understand why and check if BlocSelector work as expected
  *
- * ```dart
- * BlocSelector<BlocA, BlocAState, SelectedState>(
- *   selector: (state) {
- *     // return selected state based on the provided state.
- *   },
- *   builder: (context, state) {
- *     // return widget here based on the selected state.
- *   },
- * )
- * ```
  * {@endtemplate}
  */
 @Composable
 public inline fun <reified BlocA: BlocBase<BlocAState>,BlocAState:Any,BlockSelectedState : Any> BlocSelector(
     blocTag:String?=null,
-    crossinline selector:BlocWidgetSelector<BlocAState,BlockSelectedState>,
+    crossinline selector:BlocSelectorFn<BlocAState,BlockSelectedState>,
     content:@Composable (BlockSelectedState)->Unit)
 {
     rememberProvidedBlocOf<BlocA>(blocTag)?.let { b->
@@ -43,24 +42,28 @@ public inline fun <reified BlocA: BlocBase<BlocAState>,BlocAState:Any,BlockSelec
 }
 
 /**
- * since here we use an externally provided bloc, this is a composable that can be called
- * in any place not just a in [Screen.Content] member function
+ * same as previous method but with explicitely specified bloc instance [externallyProvidedBlock]
+ * not retrieved implicitely from current registered blocs in the current composable subtree
+ * see [BlocProvider]
  */
 @Composable
 public inline fun <reified BlocA:BlocBase<BlocAState>,BlocAState:Any,BlockSelectedState:Any> BlocSelector(
     externallyProvidedBlock:BlocA,
-    crossinline selector:BlocWidgetSelector<BlocAState,BlockSelectedState>,
+    crossinline selector:BlocSelectorFn<BlocAState,BlockSelectedState>,
     content:@Composable (BlockSelectedState)->Unit)
 {
     val b =  remember { externallyProvidedBlock }
     BlocSelectorCore(b, selector, content)
 }
 
+//TODO: check again implementation of BlocSelectCore: how it is checked for new changed selected values:
+//      by equality? by reference? it is checked at all? perhaps I should integrate with reduks selector
+//      that give more flexibility in building the selector?
 @PublishedApi
 @Composable
 internal inline fun <reified BlocA : BlocBase<BlocAState>, BlocAState : Any, BlockSelectedState : Any> BlocSelectorCore(
     b: BlocA,
-    crossinline selector: BlocWidgetSelector<BlocAState, BlockSelectedState>,
+    crossinline selector: BlocSelectorFn<BlocAState, BlockSelectedState>,
     content: @Composable (BlockSelectedState) -> Unit
 ) {
     val collect_scope = rememberCoroutineScope()
@@ -74,114 +77,3 @@ internal inline fun <reified BlocA : BlocBase<BlocAState>, BlocAState : Any, Blo
 }
 
 
-
-
-/*
-import 'package:flutter/widgets.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-/// Signature for the `selector` function which
-/// is responsible for returning a selected value, [T], based on [state].
-typedef BlocWidgetSelector<S, T> = T Function(S state);
-
-/// {@template bloc_selector}
-/// [BlocSelector] is analogous to [BlocBuilder] but allows developers to
-/// filter updates by selecting a new value based on the bloc state.
-/// Unnecessary builds are prevented if the selected value does not change.
-///
-/// **Note**: the selected value must be immutable in order for [BlocSelector]
-/// to accurately determine whether [builder] should be called again.
-///
-/// ```dart
-/// BlocSelector<BlocA, BlocAState, SelectedState>(
-///   selector: (state) {
-///     // return selected state based on the provided state.
-///   },
-///   builder: (context, state) {
-///     // return widget here based on the selected state.
-///   },
-/// )
-/// ```
-/// {@endtemplate}
-class BlocSelector<B extends StateStreamable<S>, S, T> extends StatefulWidget {
-  /// {@macro bloc_selector}
-  const BlocSelector({
-    Key? key,
-    required this.selector,
-    required this.builder,
-    this.bloc,
-  }) : super(key: key);
-
-  /// The [bloc] that the [BlocSelector] will interact with.
-  /// If omitted, [BlocSelector] will automatically perform a lookup using
-  /// [BlocProvider] and the current [BuildContext].
-  final B? bloc;
-
-  /// The [builder] function which will be invoked
-  /// when the selected state changes.
-  /// The [builder] takes the [BuildContext] and selected `state` and
-  /// must return a widget.
-  /// This is analogous to the [builder] function in [BlocBuilder].
-  final BlocWidgetBuilder<T> builder;
-
-  /// The [selector] function which will be invoked on each widget build
-  /// and is responsible for returning a selected value of type [T] based on
-  /// the current state.
-  final BlocWidgetSelector<S, T> selector;
-
-  @override
-  State<BlocSelector<B, S, T>> createState() => _BlocSelectorState<B, S, T>();
-}
-
-class _BlocSelectorState<B extends StateStreamable<S>, S, T>
-    extends State<BlocSelector<B, S, T>> {
-  late B _bloc;
-  late T _state;
-
-  @override
-  void initState() {
-    super.initState();
-    _bloc = widget.bloc ?? context.read<B>();
-    _state = widget.selector(_bloc.state);
-  }
-
-  @override
-  void didUpdateWidget(BlocSelector<B, S, T> oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    final oldBloc = oldWidget.bloc ?? context.read<B>();
-    final currentBloc = widget.bloc ?? oldBloc;
-    if (oldBloc != currentBloc) {
-      _bloc = currentBloc;
-      _state = widget.selector(_bloc.state);
-    }
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    final bloc = widget.bloc ?? context.read<B>();
-    if (_bloc != bloc) {
-      _bloc = bloc;
-      _state = widget.selector(_bloc.state);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (widget.bloc == null) {
-      // Trigger a rebuild if the bloc reference has changed.
-      // See https://github.com/felangel/bloc/issues/2127.
-      context.select<B, bool>((bloc) => identical(_bloc, bloc));
-    }
-    return BlocListener<B, S>(
-      bloc: _bloc,
-      listener: (context, state) {
-        final selectedState = widget.selector(state);
-        if (_state != selectedState) setState(() => _state = selectedState);
-      },
-      child: widget.builder(context, _state),
-    );
-  }
-}
-
- */

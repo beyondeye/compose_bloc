b6a31cebd7ef90535926c37805af4d1187be6ab1
Dario Elyasy <dario-e@beyond-eye.com>
2022-08-02 17:20:20 +0300

    finalized javadocs for utils.kt

--- kbloc_core/src/commonTest/kotlin/com/beyondeye/kbloc/stream/restartable_stream_bloc.kt
+++ kbloc_core/src/commonTest/kotlin/com/beyondeye/kbloc/stream/restartable_stream_bloc.kt
@@ -2,7 +2,6 @@ package com.beyondeye.kbloc.stream
 
 import com.beyondeye.kbloc.core.Bloc
 import com.beyondeye.kbloc.core.await
-import com.beyondeye.kbloc.core.catchError
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flatMapLatest
@@ -34,6 +33,67 @@ class OnEachCatchError :RestartableStreamEvent
 class UnawaitedOnEach :RestartableStreamEvent
 
 const val _delay_msecs= 100L
+
+/** Handles errors emitted by this [Job].
+ *
+ * This is the asynchronous equivalent of a "catch" block.
+ *
+ * Returns a new [Job] that will be completed with either the result of
+ * this one or the result of calling the `onError` callback.
+ *
+ * If this [Job] completes with a value,
+ * the returned [Job] completes with the same value.
+ *
+ * If this [Job] completes with an error,
+ * then [test] is first called with the error value.
+ *
+ * If [test] returns false, the exception is not handled by this [catchError],
+ * and the returned future completes with the same error and stack trace
+ * as this future.
+ *
+ * If [test] returns `true`,
+ * [onError] is called with the error and possibly stack trace,
+ * and the returned Job is completed with the result of this call
+ * in exactly the same way as for [then]'s `onError`.
+ *
+ * If `test` is omitted, it defaults to a function that always returns true.
+ * The `test` function should not throw, but if it does, it is handled as
+ * if the `onError` function had thrown.
+ *
+ * Note that futures don't delay reporting of errors until listeners are
+ * added. If the first `catchError` (or `then`) call happens after this future
+ * has completed with an error then the error is reported as unhandled error.
+ * See the description on [Future].
+ *
+ * Example:
+ * ```dart
+ * Future.delayed(
+ *   const Duration(seconds: 1),
+ *   () => throw 401,
+ * ).then((value) {
+ *   throw 'Unreachable';
+ * }).catchError((err) {
+ *   print('Error: $err'); // Prints 401.
+ * }, test: (error) {
+ *   return error is int && error >= 400;
+ * });
+ * ```
+// The `Function` below stands for one of two types:
+// - (dynamic) -> FutureOr<T>
+// - (dynamic, StackTrace) -> FutureOr<T>
+// Given that there is a `test` function that is usually used to do an
+// `is` check, we should also expect functions that take a specific argument.
+ */
+public fun Job.catchError(test:((Throwable)->Boolean)?=null,onError:(Throwable)->Unit ):Job {
+    invokeOnCompletion { cause ->
+        if(cause==null || cause is CancellationException) return@invokeOnCompletion
+        test?.let { if(!test(cause)) return@invokeOnCompletion }
+        //TODO when onError is run, the result of onError should be returned by this job in stead of the throwed exception
+        TODO()
+        onError(cause)
+    }
+    return this
+}
 
 class  RestartableStreamBloc(cscope:CoroutineScope,val inputEventsStream: Flow<Int>) :Bloc<RestartableStreamEvent,Int>(cscope,0,false) {
     init {

226a87df563664978a392942fdbfc4b82c24f16a
Dario Elyasy <dario-e@beyond-eye.com>
2022-08-04 16:52:44 +0300

    ScreenLifecycle.kt made deprecated with error since it is not working

--- kbloc_compose/src/main/kotlin/com/beyondeye/kbloc/compose/lifecycle/ScreenLifecycle.kt
+++ kbloc_compose/src/main/kotlin/com/beyondeye/kbloc/compose/lifecycle/ScreenLifecycle.kt
@@ -5,7 +5,20 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import com.beyondeye.kbloc.compose.screen.Screen
 
+//
+//
+/**
+ * *DARIO* this is simply a disposable effect for the screen and it is not doing what is expected
+ * see for example https://github.com/adrielcafe/voyager/issues/42
+ * see also https://voyager.adriel.cafe/lifecycle
+ * So now it is deprecated
+ * the correct way to listen to Lifecyle events is as described here:
+ * https://developer.android.com/jetpack/compose/side-effects#disposableeffect
+ */
 @Composable
+@Deprecated(
+    "Not Working, substitute with what described at https://developer.android.com/jetpack/compose/side-effects#disposableeffect",
+    level=DeprecationLevel.ERROR)
 public fun Screen.LifecycleEffect(
     onStarted: () -> Unit = {},
     onDisposed: () -> Unit = {}

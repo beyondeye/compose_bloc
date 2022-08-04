3779a00ae7e5e328a5b5245c713298d6b4f48396
Dario Elyasy <dario-e@beyond-eye.com>
2022-08-04 17:28:23 +0300

    updated NavigatorSaver.kt for additional Navigator fields: screenModelStore and blocStore

--- kbloc_compose/src/main/kotlin/com/beyondeye/kbloc/compose/navigator/internals/NavigatorSaver.kt
+++ kbloc_compose/src/main/kotlin/com/beyondeye/kbloc/compose/navigator/internals/NavigatorSaver.kt
@@ -7,6 +7,10 @@ import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.staticCompositionLocalOf
+import com.beyondeye.kbloc.compose.bloc.internals.BlocStore
+import com.beyondeye.kbloc.compose.bloc.internals.LocalBlocStoreOwner
+import com.beyondeye.kbloc.compose.model.ScreenModelStore
+import com.beyondeye.kbloc.compose.model.internals.LocalScreenModelStoreOwner
 import com.beyondeye.kbloc.compose.navigator.Navigator
 import com.beyondeye.kbloc.compose.navigator.NavigatorDisposeBehavior
 import com.beyondeye.kbloc.compose.screen.Screen
@@ -21,18 +25,22 @@ internal fun rememberNavigator(
     parent: Navigator?
 ): Navigator {
     val stateHolder = LocalNavigatorStateHolder.current
+    val screenModelStore = LocalScreenModelStoreOwner.current.screenModelStore
+    val blocStore = LocalBlocStoreOwner.current.blocStore
 
-    return rememberSaveable(saver = navigatorSaver(stateHolder, disposeBehavior, parent)) {
-        Navigator(screens, stateHolder, disposeBehavior, parent)
+    return rememberSaveable(saver = navigatorSaver(stateHolder, disposeBehavior, parent,screenModelStore,blocStore)) {
+        Navigator(screens, stateHolder, disposeBehavior, parent,screenModelStore,blocStore)
     }
 }
 
 private fun navigatorSaver(
     stateHolder: SaveableStateHolder,
     disposeBehavior: NavigatorDisposeBehavior,
-    parent: Navigator?
+    parent: Navigator?,
+    screenModelStore: ScreenModelStore,
+    blocStore: BlocStore
 ): Saver<Navigator, Any> =
     listSaver(
         save = { navigator -> navigator.items },
-        restore = { items -> Navigator(items, stateHolder, disposeBehavior, parent) }
+        restore = { items -> Navigator(items, stateHolder, disposeBehavior, parent,screenModelStore,blocStore) }
     )

15029726f38f26b16402a31e8cda7e9e03d8a007
Dario Elyasy <dario-e@beyond-eye.com>
2022-08-04 16:54:54 +0300

    NavigatorDisposable.kt code commented out because it was not working

--- kbloc_compose/src/main/kotlin/com/beyondeye/kbloc/compose/navigator/internals/NavigatorDisposable.kt
+++ kbloc_compose/src/main/kotlin/com/beyondeye/kbloc/compose/navigator/internals/NavigatorDisposable.kt
@@ -8,6 +8,15 @@ import com.beyondeye.kbloc.compose.stack.StackEvent
 private val disposableEvents: Set<StackEvent> =
     setOf(StackEvent.Pop, StackEvent.Replace)
 
+/**
+ * *DARIO* this was not working, a Screen must be disposed only when it is
+ * explicitely removed (call to [Navigator.pop]) not if it leaves the composition
+ * otherwise in android where a screen can be removed from composition if for
+ * example the device is rotated, this will cause a lot of problems with screenmodels
+ * that are destroyed and recreated without any need:  see for example
+ * https://github.com/adrielcafe/voyager/issues/62
+ */
+/*
 @Composable
 internal fun NavigatorDisposableEffect(
     navigator: Navigator
@@ -21,7 +30,13 @@ internal fun NavigatorDisposableEffect(
         }
     }
 }
+ */
 
+/**
+ * *DARIO* I am not sure what this was for but is for sure not working, like NavigatorDisposableEffect
+ * see the comment above
+ */
+/*
 @Composable
 internal fun StepDisposableEffect(
     navigator: Navigator
@@ -37,3 +52,4 @@ internal fun StepDisposableEffect(
         }
     }
 }
+ */

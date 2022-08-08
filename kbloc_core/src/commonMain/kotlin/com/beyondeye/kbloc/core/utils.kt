e52f20ba473e5670c86fa12e8ce3adb2fb9258bf
Dario Elyasy <dario-e@beyond-eye.com>
2022-08-08 10:12:14 +0300

    added logtag const

--- kbloc_core/src/commonMain/kotlin/com/beyondeye/kbloc/core/utils.kt
+++ kbloc_core/src/commonMain/kotlin/com/beyondeye/kbloc/core/utils.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.datetime.Clock
+public const val LOGTAG="*kbloc*"
 
 public fun <T> Iterable<Deferred<T>>.awaitAny(scope: CoroutineScope): CompletableDeferred<T> {
     val completer = CompletableDeferred<T>()

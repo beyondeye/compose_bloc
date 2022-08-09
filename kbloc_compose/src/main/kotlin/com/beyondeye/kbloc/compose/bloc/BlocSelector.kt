95090b2d523f7b00129267dce8da48ed8de00b7c
Dario Elyasy <dario-e@beyond-eye.com>
2022-08-09 17:34:59 +0300

    BlocSelector: initial integration with AbstractSelector

--- kbloc_compose/src/main/kotlin/com/beyondeye/kbloc/compose/bloc/BlocSelector.kt
+++ kbloc_compose/src/main/kotlin/com/beyondeye/kbloc/compose/bloc/BlocSelector.kt
@@ -1,9 +1,10 @@
 package com.beyondeye.kbloc.compose.bloc
 
 import androidx.compose.runtime.*
-import com.beyondeye.kbloc.compose.screen.Screen
+import com.beyondeye.kbloc.compose.bloc.reselect.AbstractSelector
 import com.beyondeye.kbloc.core.BlocBase
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.mapNotNull
 
 /**
  * Signature for the `selector` function which
@@ -11,7 +12,7 @@ import kotlinx.coroutines.flow.map
  * NOTE: this was named BlocWidgetSelector in flutter_bloc
  * TODO: integrate BlocSelectorFn with reduks selectors that automatically check for state changes before emitting selected state
  */
-typealias BlocSelectorFn<S, T> = (S)->T
+typealias BlocSelectorFn<S, T> = @DisallowComposableCalls (S)->T
 
 /**
  *
@@ -33,14 +34,28 @@ typealias BlocSelectorFn<S, T> = (S)->T
 @Composable
 public inline fun <reified BlocA: BlocBase<BlocAState>,BlocAState:Any,BlockSelectedState : Any> BlocSelector(
     blocTag:String?=null,
-    crossinline selector:BlocSelectorFn<BlocAState,BlockSelectedState>,
+    crossinline selectorFn: BlocSelectorFn<BlocAState,BlockSelectedState>,
     content:@Composable (BlockSelectedState)->Unit)
 {
     rememberProvidedBlocOf<BlocA>(blocTag)?.let { b->
-        BlocSelectorCore(b, selector, content)
+        BlocSelectorForSelectorFn(b, selectorFn, content)
     }
 }
 
+
+/**
+ * same as above but take a parameter [select] that is a method that build an [AbstractSelector]
+ */
+@Composable
+public inline fun <reified BlocA: BlocBase<BlocAState>,BlocAState:Any,BlockSelectedState : Any> BlocSelector(
+    blocTag:String?=null,
+    select:@DisallowComposableCalls () -> AbstractSelector<BlocAState,BlockSelectedState>,
+    content:@Composable (BlockSelectedState)->Unit)
+{
+    rememberProvidedBlocOf<BlocA>(blocTag)?.let { b->
+        BlocSelectorForAbstractSelector(b, select, content)
+    }
+}
 /**
  * same as previous method but with explicitely specified bloc instance [externallyProvidedBlock]
  * not retrieved implicitely from current registered blocs in the current composable subtree
@@ -49,19 +64,33 @@ public inline fun <reified BlocA: BlocBase<BlocAState>,BlocAState:Any,BlockSelec
 @Composable
 public inline fun <reified BlocA:BlocBase<BlocAState>,BlocAState:Any,BlockSelectedState:Any> BlocSelector(
     externallyProvidedBlock:BlocA,
-    crossinline selector:BlocSelectorFn<BlocAState,BlockSelectedState>,
+    crossinline selectorFn:BlocSelectorFn<BlocAState,BlockSelectedState>,
     content:@Composable (BlockSelectedState)->Unit)
 {
     val b =  remember { externallyProvidedBlock }
-    BlocSelectorCore(b, selector, content)
+    BlocSelectorForSelectorFn(b, selectorFn, content)
+}
+
+/**
+ * same as above but take a parameter [select] that is a method that build an [AbstractSelector]
+ */
+@Composable
+public inline fun <reified BlocA:BlocBase<BlocAState>,BlocAState:Any,BlockSelectedState:Any> BlocSelector(
+    externallyProvidedBlock:BlocA,
+    select:@DisallowComposableCalls () -> AbstractSelector<BlocAState,BlockSelectedState>,
+    content:@Composable (BlockSelectedState)->Unit)
+{
+    val b =  remember { externallyProvidedBlock }
+    BlocSelectorForAbstractSelector(b, select, content)
 }
 
 //TODO: check again implementation of BlocSelectCore: how it is checked for new changed selected values:
 //      by equality? by reference? it is checked at all? perhaps I should integrate with reduks selector
 //      that give more flexibility in building the selector?
+//      DOCUMENT THAT FOR MORE SOPHISTICATED CHECK SHOULD USE AbstractSelector
 @PublishedApi
 @Composable
-internal inline fun <reified BlocA : BlocBase<BlocAState>, BlocAState : Any, BlockSelectedState : Any> BlocSelectorCore(
+internal inline fun <reified BlocA : BlocBase<BlocAState>, BlocAState : Any, BlockSelectedState : Any> BlocSelectorForSelectorFn(
     b: BlocA,
     crossinline selector: BlocSelectorFn<BlocAState, BlockSelectedState>,
     content: @Composable (BlockSelectedState) -> Unit
@@ -69,6 +98,29 @@ internal inline fun <reified BlocA : BlocBase<BlocAState>, BlocAState : Any, Blo
     val collect_scope = rememberCoroutineScope()
     val stream = b.stream.map { selector(it) }
     val initialState=selector(b.state)
+    val state: BlockSelectedState by stream.collectAsState(
+        initialState,
+        collect_scope.coroutineContext
+    )
+    content(state)
+}
+
+/**
+ * parameter [select] is a selector builder method
+ */
+@PublishedApi
+@Composable
+internal inline fun <reified BlocA : BlocBase<BlocAState>, BlocAState : Any, BlockSelectedState : Any> BlocSelectorForAbstractSelector(
+    b: BlocA,
+    select: @DisallowComposableCalls () -> AbstractSelector<BlocAState, BlockSelectedState>,
+    content: @Composable (BlockSelectedState) -> Unit
+) {
+    val collect_scope = rememberCoroutineScope()
+    //store selector and reuse it on recomposition. no need to use rememberSaveable: when screen is
+    // destroyed and rebuilt we probably want to trigger all our selector
+    val sel = remember { select() }
+    val stream = b.stream.mapNotNull { sel.getIfChangedIn(it) }
+    val initialState=sel(b.state)
     val state: BlockSelectedState by stream.collectAsState(
         initialState,
         collect_scope.coroutineContext

b0fbb5eefffd61cccc91f0896842bef4a428b73f
Dario Elyasy <dario-e@beyond-eye.com>
2022-08-09 12:22:31 +0300

    refactored CounterBloc as separate file

---
+++ kbloc_app/src/main/java/com/beyondeye/kbloc_app/CounterBloc.kt
@@ -0,0 +1,35 @@
+package com.beyondeye.kbloc_app
+
+import com.beyondeye.kbloc.core.Bloc
+import kotlinx.coroutines.CoroutineScope
+
+interface CounterEvent
+object IncrementEvent:CounterEvent
+object DecrementEvent:CounterEvent
+class AdditionEvent(val value:Int):CounterEvent
+class SubtractionEvent(val value:Int):CounterEvent
+data class CounterState(val counter:Int=0)
+
+class CounterBloc(cscope: CoroutineScope, startCounter:Int=0): Bloc<CounterEvent, CounterState>(cscope,
+    CounterState(startCounter),false
+) {
+    init {
+        on<IncrementEvent> { event, emit ->
+            val s=state
+            emit(s.copy(counter =s.counter+1 ))
+        }
+        on<AdditionEvent> { event, emit ->
+            val s=state
+            emit(s.copy(counter =s.counter+event.value ))
+        }
+        on<DecrementEvent> { event, emit ->
+            val s=state
+            emit(s.copy(counter =s.counter-1 ))
+        }
+        on<SubtractionEvent> { event, emit ->
+            val s=state
+            emit(s.copy(counter =s.counter-event.value ))
+        }
+    }
+
+}

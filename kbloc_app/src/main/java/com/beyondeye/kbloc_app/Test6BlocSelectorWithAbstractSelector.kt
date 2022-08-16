d26b3ad43b4a9b5280f3558f84d2ce358776edf5
Dario Elyasy <dario-e@beyond-eye.com>
2022-08-16 10:57:11 +0300

    updated Test6BlocSelectorWithAbstractSelector.kt

--- kbloc_app/src/main/java/com/beyondeye/kbloc_app/Test6BlocSelectorWithAbstractSelector.kt
+++ kbloc_app/src/main/java/com/beyondeye/kbloc_app/Test6BlocSelectorWithAbstractSelector.kt
@@ -12,7 +12,7 @@ import com.beyondeye.kbloc.compose.bloc.reselect.SelectorBuilder
 import com.beyondeye.kbloc.compose.screen.Screen
 
 
-class Test6BlocSelectorWithAbstractSelector: Screen {
+class Test6BlocSelectorWithAbstractSelector : Screen {
     @Composable
     override fun Content() {
         Test6ScreenContent()
@@ -20,55 +20,76 @@ class Test6BlocSelectorWithAbstractSelector: Screen {
 
     @Composable
     private fun Test6ScreenContent() {
-        Column(modifier=Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                BlocProvider(create={cscope ->  ABCCounterBloc(cscope, Array(3){0})})
-                {
-                    //rememberProvidedBlocOf is similar to dependency injection: it retrieves the specified
-                    //bloc type as defined by the closest enclosing BlocProvider
-                    val bloc= rememberProvidedBlocOf<ABCCounterBloc>()?:return@BlocProvider
-                    //TODO should I use remember for definition of all these callbacks?
-                    //     (also in other tests)
-                    val onIncr_a =remember { {
-                        bloc.add(MultiAddEvent(0,1))
-                    } }
-                    val onIncr_b = {
-                        bloc.add(MultiAddEvent(1,1))
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            BlocProvider(create = { cscope -> ABCCounterBloc(cscope, Array(3) { 0 }) })
+            {
+                //rememberProvidedBlocOf is similar to dependency injection: it retrieves the specified
+                //bloc type as defined by the closest enclosing BlocProvider
+                val bloc = rememberProvidedBlocOf<ABCCounterBloc>() ?: return@BlocProvider
+                //TODO should I use remember for definition of all these callbacks?
+                //     (also in other tests)
+                val onIncr_a = remember {
+                    {
+                        bloc.add(MultiAddEvent(0, 1))
                     }
-                    val onIncr_c = {
-                        bloc.add(MultiAddEvent(2,1))
+                }
+                val onIncr_b = remember {
+                    {
+                        bloc.add(MultiAddEvent(1, 1))
                     }
-                    val onDecr_a = {
-                        bloc.add(MultiSubEvent(0,1))
+                }
+                val onIncr_c = remember {
+                    {
+                        bloc.add(MultiAddEvent(2, 1))
                     }
-                    val onDecr_b = {
-                        bloc.add(MultiSubEvent(1,1))
+                }
+                val onDecr_a = remember {
+                    {
+                        bloc.add(MultiSubEvent(0, 1))
                     }
-                    val onDecr_c = {
-                        bloc.add(MultiSubEvent(2,1))
+                }
+                val onDecr_b = remember {
+                    {
+                        bloc.add(MultiSubEvent(1, 1))
                     }
-                    val selb=SelectorBuilder<ABCCounterState>()
-                    //2nd and third template argument types are inferred automatically
-                    BlocSelector<ABCCounterBloc,_,_>(select = { selb.withSingleField { a } })
-                    { counterValue->
-                        CounterControls("a",counterValue, onDecr_a,onIncr_a)
-
+                }
+                val onDecr_c = remember {
+                    {
+                        bloc.add(MultiSubEvent(2, 1))
                     }
-                    Divider(modifier = Modifier.height(2.dp))
-                    //2nd and third template argument types are inferred automatically
-                    BlocSelector<ABCCounterBloc,_,_>(select = { selb.withSingleField { b } })
-                    { counterValue->
-                        CounterControls("b",counterValue, onDecr_b,onIncr_b)
-
-                    }
-                    Divider(modifier = Modifier.height(2.dp))
-                    //2nd and third template argument types are inferred automatically
-                    BlocSelector<ABCCounterBloc,_,_>(select = { selb.withSingleField { c } })
-                    { counterValue->
-                        CounterControls("c",counterValue, onDecr_c,onIncr_c)
-
-                    }
+                }
+                val selb = SelectorBuilder<ABCCounterState>()
+                BlocSelector(
+                    bloc,
+                    selb.withField { a }.withField { b }.withField { c }
+                        .compute { a, b, c -> a + b + c }) {
+                    Text("total count=$it")
 
                 }
+                //2nd and third template argument types are inferred automatically
+                BlocSelector<ABCCounterBloc, _, _>(selector = selb.withSingleField { a })
+                { counterValue ->
+                    CounterControls("a", counterValue, onDecr_a, onIncr_a)
+
+                }
+                Divider(modifier = Modifier.height(2.dp))
+                //if we explicitely specify the bloc parameter we don't need to write the template args at all!
+                BlocSelector(bloc, selb.withSingleField { b })
+                { counterValue ->
+                    CounterControls("b", counterValue, onDecr_b, onIncr_b)
+
+                }
+                Divider(modifier = Modifier.height(2.dp))
+                //if we explicitely specify the bloc parameter we don't need to write the template args at all!
+                BlocSelector(bloc, selb.withSingleField { c })
+                { counterValue ->
+                    CounterControls("c", counterValue, onDecr_c, onIncr_c)
+                }
+
+            }
 
         }
     }

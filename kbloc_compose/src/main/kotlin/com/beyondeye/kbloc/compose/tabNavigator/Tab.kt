d189bcca16f968713cd9ce4d5c3d12ba5c252577
Dario Elyasy <dario-e@beyond-eye.com>
2022-08-17 10:05:01 +0300

    added TabNavigator code

---
+++ kbloc_compose/src/main/kotlin/com/beyondeye/kbloc/compose/tabNavigator/Tab.kt
@@ -0,0 +1,41 @@
+package com.beyondeye.kbloc.compose.tabNavigator
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+import com.beyondeye.kbloc.compose.screen.Screen
+
+@Composable
+public fun CurrentTab() {
+    val tabNavigator = LocalTabNavigator.current
+    val currentTab = tabNavigator.current
+
+    tabNavigator.saveableState("currentTab") {
+        currentTab.Content()
+    }
+}
+
+public data class TabOptions(
+    val index: UShort,
+    val title: String,
+    val icon: Painter? = null
+)
+
+public interface Tab : Screen {
+
+    public val options: TabOptions
+        @Composable get
+
+    @Deprecated(
+        message = "Use 'options' instead. Will be removed in 1.0.0.",
+        replaceWith = ReplaceWith("options")
+    )
+    public val title: String
+        @Composable get() = options.title
+
+    @Deprecated(
+        message = "Use 'options' instead. Will be removed in 1.0.0.",
+        replaceWith = ReplaceWith("options")
+    )
+    public val icon: Painter?
+        @Composable get() = options.icon
+}
---
+++ kbloc_compose/src/main/kotlin/com/beyondeye/kbloc/compose/tabNavigator/TabNavigator.kt
@@ -0,0 +1,55 @@
+package com.beyondeye.kbloc.compose.tabNavigator
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ProvidableCompositionLocal
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
+import com.beyondeye.kbloc.compose.navigator.Navigator
+import com.beyondeye.kbloc.compose.navigator.NavigatorDisposeBehavior
+
+public typealias TabNavigatorContent = @Composable (tabNavigator: TabNavigator) -> Unit
+
+public val LocalTabNavigator: ProvidableCompositionLocal<TabNavigator> =
+    staticCompositionLocalOf { error("TabNavigator not initialized") }
+
+@Composable
+public fun TabNavigator(
+    tab: Tab,
+    disposeNestedNavigators: Boolean = false,
+    content: TabNavigatorContent = { CurrentTab() }
+) {
+    Navigator(
+        screen = tab,
+        disposeBehavior = NavigatorDisposeBehavior(
+            disposeNestedNavigators = disposeNestedNavigators,
+            disposeSteps = false
+        ),
+        onBackPressed = null
+    ) { navigator ->
+        val tabNavigator = remember(navigator) {
+            TabNavigator(navigator)
+        }
+
+        CompositionLocalProvider(LocalTabNavigator provides tabNavigator) {
+            content(tabNavigator)
+        }
+    }
+}
+
+public class TabNavigator internal constructor(
+    private val navigator: Navigator
+) {
+
+    public var current: Tab
+        get() = navigator.lastItem as Tab
+        set(tab) = navigator.replaceAll(tab)
+
+    @Composable
+    public fun saveableState(
+        key: String,
+        content: @Composable () -> Unit
+    ) {
+        navigator.saveableState(key, content = content)
+    }
+}

4fbca50131b00eed09a2827034819849a2c9d441
Dario Elyasy <dario-e@beyond-eye.com>
2022-08-17 10:03:58 +0300

    added in comments expect declarations for code in concurrent package

--- kbloc_compose/src/main/kotlin/com/beyondeye/kbloc/compose/concurrent/AtomicInt32.kt
+++ kbloc_compose/src/main/kotlin/com/beyondeye/kbloc/compose/concurrent/AtomicInt32.kt
@@ -1,4 +1,10 @@
 package com.beyondeye.kbloc.compose.concurrent
 
+/**
+    public expect class AtomicInt32(initialValue: Int) {
+        public fun getAndIncrement(): Int
+    }
+ */
+
 //public actual typealias AtomicInt32 = java.util.concurrent.atomic.AtomicInteger
 typealias AtomicInt32 = java.util.concurrent.atomic.AtomicInteger
--- kbloc_compose/src/main/kotlin/com/beyondeye/kbloc/compose/concurrent/ThreadSafeList.kt
+++ kbloc_compose/src/main/kotlin/com/beyondeye/kbloc/compose/concurrent/ThreadSafeList.kt
@@ -2,5 +2,9 @@ package com.beyondeye.kbloc.compose.concurrent
 
 import java.util.concurrent.CopyOnWriteArrayList
 
+/*
+    public expect class ThreadSafeList<T>() : MutableList<T>
+ */
+
 //public actual class ThreadSafeList<T> : MutableList<T> by CopyOnWriteArrayList()
 typealias  ThreadSafeList<T> = CopyOnWriteArrayList<T>

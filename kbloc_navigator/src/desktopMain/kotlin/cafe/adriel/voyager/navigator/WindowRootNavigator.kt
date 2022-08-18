240cb03ee82d14858d784b936a3fe14e2ef466ee
Dario Elyasy <dario-e@beyond-eye.com>
2022-08-18 09:20:33 +0300

    added WindowRootNavigator.kt

---
+++ kbloc_navigator/src/desktopMain/kotlin/cafe/adriel/voyager/navigator/WindowRootNavigator.kt
@@ -0,0 +1,20 @@
+package cafe.adriel.voyager.navigator
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.window.FrameWindowScope
+import cafe.adriel.voyager.core.screen.Screen
+
+/**
+ * RootNavigator for desktop need to defined at the Window level
+ */
+@Composable
+public fun FrameWindowScope.RootNavigator(
+    screens: List<Screen>,
+    disposeBehavior: NavigatorDisposeBehavior = NavigatorDisposeBehavior(),
+    onBackPressed: OnBackPressed = { true },
+    content: NavigatorContent = { CurrentScreen() }
+) {
+    //TODO *DARIO* implement lifecycle management similar to what is done for android RootNavigator
+    // see https://github.com/JetBrains/compose-jb/tree/master/tutorials/Window_API_new
+    Navigator(screens,disposeBehavior,onBackPressed,content)
+}

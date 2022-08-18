eda158d18e9bef0eae964d1b5537e5ecce0d4135
Dario Elyasy <dario-e@beyond-eye.com>
2022-08-18 09:35:52 +0300

    removed concurrent package from kbloc_compose

--- kbloc_compose/src/main/kotlin/com/beyondeye/kbloc/compose/concurrent/ThreadSafeMap.kt
+++
@@ -1,9 +0,0 @@
-package com.beyondeye.kbloc.compose.concurrent
-
-import java.util.concurrent.ConcurrentHashMap
-/*
-    public expect class ThreadSafeMap<K, V>() : MutableMap<K, V>
- */
-
-//public actual class ThreadSafeMap<K, V> : MutableMap<K, V> by ConcurrentHashMap()
-typealias ThreadSafeMap<K, V> = ConcurrentHashMap<K,V>

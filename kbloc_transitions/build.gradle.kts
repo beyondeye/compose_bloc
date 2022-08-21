2e1508a0d99e85cf5691011b268da5e62da1b51c
Dario Elyasy <dario-e@beyond-eye.com>
2022-08-21 12:31:55 +0300

    added kbloc_transitions library

---
+++ kbloc_transitions/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    id("org.jetbrains.compose")
+    id("com.vanniktech.maven.publish")
+}
+
+setupModuleForComposeMultiplatform()
+
+kotlin {
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                api(project(":kbloc_navigator"))
+                compileOnly(compose.animation)
+            }
+        }
+
+        val jvmTest by getting {
+            dependencies {
+  //              implementation(libs.junit.api)
+   //             runtimeOnly(libs.junit.engine)
+            }
+        }
+
+        val androidTest by getting {
+            dependencies {
+//                implementation(compose.runtime)
+//                implementation(compose.ui)
+            }
+        }
+
+        val androidMain by getting {
+            dependencies {
+//                implementation("androidx.activity:activity-compose:${Versions.compose_activity_version}")
+            }
+        }
+    }
+}

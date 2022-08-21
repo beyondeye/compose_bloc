9c90a7152749e3576926435d5d2e30680e865eb8
Dario Elyasy <dario-e@beyond-eye.com>
2022-08-21 12:17:23 +0300

    added kbloc_bottomsheetnavigator and kbloc_tabnavigator libraries

---
+++ kbloc_bottom_sheet_navigator/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    id("org.jetbrains.compose")
+    id("com.vanniktech.maven.publish")
+}
+
+setupModuleForComposeMultiplatform()
+
+kotlin {
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                api(project(":kbloc_navigator"))
+                compileOnly(compose.runtime)
+                compileOnly(compose.material)
+            }
+        }
+
+        val jvmTest by getting {
+            dependencies {
+  //              implementation(libs.junit.api)
+   //             runtimeOnly(libs.junit.engine)
+            }
+        }
+
+        val androidTest by getting {
+            dependencies {
+                implementation(compose.runtime)
+                implementation(compose.ui)
+            }
+        }
+
+        val androidMain by getting {
+            dependencies {
+                implementation("androidx.activity:activity-compose:${Versions.compose_activity_version}")
+            }
+        }
+    }
+}

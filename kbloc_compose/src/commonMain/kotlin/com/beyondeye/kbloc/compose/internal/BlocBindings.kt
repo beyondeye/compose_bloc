4a729551bc506519225e69efe32f8ab9757160e2
Dario Elyasy <dario-e@beyond-eye.com>
2022-08-25 13:12:48 +0300

    small fix to BlocBindings.kt for js support

--- kbloc_compose/src/commonMain/kotlin/com/beyondeye/kbloc/compose/internal/BlocBindings.kt
+++ kbloc_compose/src/commonMain/kotlin/com/beyondeye/kbloc/compose/internal/BlocBindings.kt
@@ -31,8 +31,7 @@ internal class BlocBindings(val bindingMaps: PersistentMap<String, String> = per
      */
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-        other as BlocBindings
+        other as? BlocBindings ?: return false
         return bindingMaps === other.bindingMaps
     }
 

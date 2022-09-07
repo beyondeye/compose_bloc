0a0e6e0f455bdb6b83a4cc6cb696e6b1dc823a6e
Dario Elyasy <dario-e@beyond-eye.com>
2022-09-07 17:50:15 +0300

    fixed javadocs for RouterWithRawPathListener.kt

--- kbloc_router/src/jsMain/kotlin/com/beyondeye/kbloc/router/RouterWithRawPathListener.kt
+++ kbloc_router/src/jsMain/kotlin/com/beyondeye/kbloc/router/RouterWithRawPathListener.kt
@@ -3,6 +3,9 @@ package com.beyondeye.kbloc.router
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 
+/**
+ * base class for [BrowserRouter] and [BrowserHashRouter]
+ */
 interface RouterWithRawPathListener:Router {
     fun setupRawPathListener(initPath: String)
     fun removeRawPathListener()

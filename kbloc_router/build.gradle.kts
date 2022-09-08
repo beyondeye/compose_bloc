5dc8b62a2c5157ebae2c215933d7780c95973665
Dario Elyasy <dario-e@beyond-eye.com>
2022-09-08 11:56:45 +0300

    added missing dep for kbloc_router

--- kbloc_router/build.gradle.kts
+++ kbloc_router/build.gradle.kts
@@ -21,8 +21,7 @@ kotlin {
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines_version}")
 //                implementation(Deps.AtomicFu.common)
                 compileOnly(compose.runtime)
-//                compileOnly("org.jetbrains.compose.runtime:runtime-saveable:${Versions.jbcompose_version}")
-
+                compileOnly("org.jetbrains.compose.runtime:runtime-saveable:${Versions.jbcompose_version}")
 //                compileOnly(compose.material)
             }
         }
